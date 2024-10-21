b2ff906556e9cb9628ff7fac72a4ad9fac28da49
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-21 01:00:38 +0000

    fix: Documentation for 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the string to the terminal
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
