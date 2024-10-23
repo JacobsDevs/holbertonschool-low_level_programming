ffaaf09f8a226895b22bb431d6ed5a26376327d7
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-23 23:18:27 +0000

    add: header for 0-putchar and solved question

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Uses printf to display "_putchar" to the terminal followed by newline
+ * without the use of any stdlibs
  *
  * Return: 0 - No error
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
