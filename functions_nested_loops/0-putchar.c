be291d4aaac1c74c07c9221fcb427126efc2f1b8
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-23 04:53:22 +0000

    add: 0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Uses printf to display "_putchar" to the terminal followed by newline
+ *
+ * Return: 0 - No error
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
