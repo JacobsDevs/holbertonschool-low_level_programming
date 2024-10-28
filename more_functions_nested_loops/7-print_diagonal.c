115bbfb7155a19a39bdcd2e60c4c2be5e5e72ae2
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-28 02:57:40 +0000

    add: 7-print_diagonals.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print \ n times spaced correctly followed by a new line
+ * @n: the number of _ to print.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(32);
+			j++;
+		}
+		_putchar(92);
+		_putchar(10);
+		i++;
+	}
+	_putchar(10);
+}
