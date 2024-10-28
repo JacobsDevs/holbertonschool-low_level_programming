a5a116cc5a80b517e23b35e870af8d8f1981ea60
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-28 03:24:18 +0000

    add: 10-print_triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle of #s followed by a new line
+ * @n: the height of the # triangle to print.
+ */
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	i = 0;
+	while (i++ < n)
+	{
+		j = 0;
+		while (j < n - i)
+		{
+			_putchar(32);
+			j++;
+		}
+		j = 0;
+		while (j < i)
+		{
+			_putchar(35);
+			j++;
+		}
+		_putchar(10);
+	}
+}
