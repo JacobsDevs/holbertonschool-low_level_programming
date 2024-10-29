c629f5c0d5c9a2556ec03f3fbcbbbfda6dd9e41c
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-29 22:49:42 +0000

    add: 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: The first letter of the string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar(10);
+}
