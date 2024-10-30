01ad8cbe09465506f5d628001bf9eebfdb334600
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-30 05:03:04 +0000

    fix: 100-atoi.c handles INT_MIN & signs correctly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 /**
  * _atoi - converts the first number in a string to an int respecting sign
  * @s: The string to convert
@@ -9,33 +10,27 @@
 
 int _atoi(char *s)
 {
-	int i, num, posneg;
+	int i, num, neg, pnsign;
 
 	i = 0;
-	posneg = 0;
-
+	neg = 0;
+	pnsign = 1;
 	while ((s[i] < 48 || s[i] > 57) && s[i] != '\0')
 	{
-		if (s[i] == '+')
-			posneg++;
-		else if (s[i] == '-')
-			posneg--;
+		if (s[i] == '-')
+			neg++;
 		i++;
 	}
+	if (s[i] == '\0')
+		return (0);
+	if (neg % 2 == 1)
+		pnsign = -1;
 	num = 0;
 	while (s[i] > 47 && s[i] < 58)
 	{
 		num *= 10;
-		num += (s[i] - 48);
+		num += ((s[i] - 48) * pnsign);
 		i++;
 	}
-	if (num > 0)
-	{
-		if (posneg < 0)
-			return (num * -1);
-		else
-			return (num);
-	}
-	else
-		return (0);
+	return (num);
 }
