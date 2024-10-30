2942f5f2d53900a4950fe0da84e0492f5aa54289
JacobsDevs <jacobsdevsmail@gmail.com>
2024-10-30 01:09:15 +0000

    add: 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the 2nd half of a string to the screen followed by
+ * a new line
+ * @str: The string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int n, i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 != 0)
+		n = ((i - 1) / 2);
+	else
+		n = i / 2;
+	while (str[n] != '\0')
+		_putchar(str[n++]);
+	_putchar(10);
+}
