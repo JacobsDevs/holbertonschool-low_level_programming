bedee178ec24898bea56d263b7bc78decbd4e547
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-04 10:17:02 +0000

    fix: 2-strchr attempt 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,6 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char *null = 0;
 
 	while (s[i] != '\0')
 	{
@@ -18,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		i++;
 	}
-	return (null);
+	return (0);
 }
