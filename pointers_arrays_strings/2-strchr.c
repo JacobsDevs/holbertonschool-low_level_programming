9d1582554ac11467cf5e718fe6555b20be28e9a0
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-04 01:43:18 +0000

    add: 2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - Returns a pointer to the first occurence of c in s or NULL if
+ * not found.
+ * @s: pointer to the start of the string
+ * @c: char to search for
+ *
+ * Return: Pointer of c in s or 0 if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
