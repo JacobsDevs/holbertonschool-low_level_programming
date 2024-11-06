616f1b7248e74195bd598078f7a5ad94b4f48f2e
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-06 03:44:02 +0000

    add: 1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Recursively uses putchar to print a string to the terminal
+ * @s: Pointer to the start of the string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
