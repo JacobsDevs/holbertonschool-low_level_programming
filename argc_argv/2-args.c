b85b2c5a060b1b1941df9b568adcb06ffce6156e
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-07 04:52:11 +0000

    add: 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the name of the file to the screen
+ * @argc: Count of CL arguments
+ * @argv: Array of CL arguments
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+	return (0);
+}
