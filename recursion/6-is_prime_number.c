d74801ff97f7c69eab1ee002c0b8956f2f5a0490
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-07 04:21:08 +0000

    add: 6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * prime_search - Works through n / 2 - 1 and searches for divisables
+ * @n: Original number
+ * @i: Current iteration
+ *
+ * Return: 1 for prime 0 for non prime
+ */
+
+int prime_search(int n, int i)
+{
+	int primer = 0;
+
+	if (i == 0)
+		return (0);
+	else if (n % i == 0)
+	{
+		printf("Found: %d is divisible by %d\n", n, i);
+		primer = 1;
+
+	}
+	return (primer + prime_search(n, i - 1));
+}
+
+/**
+ * is_prime_number - Checks to see if a number is a prime number
+ * @n: The number to check
+ *
+ * Return: 1 for prime number 0 for not prime
+ */
+
+int is_prime_number(int n)
+{
+	printf("Number is %d\n\n", n);
+	if (prime_search(n, (n / 2 + 1)) == 1)
+		return (1);
+	else
+		return (0);
+}
