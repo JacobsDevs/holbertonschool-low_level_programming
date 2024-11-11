373c131bbfc6a11b61f2eb533b93407f99248f13
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-11 01:13:16 +0000

    fix 2-calloc.c correct for 402, sizeof(char) attempt 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	array = malloc(nmemb * size);
+	array = malloc(nmemb * size + 1);
 	if (array == NULL)
 		return (NULL);
 	while (i < nmemb)
