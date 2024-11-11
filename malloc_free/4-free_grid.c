aa4d8043ee13090bbf636ee1f36d5f5761785749
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-11 00:05:53 +0000

    add: 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2d grid
+ * @grid: pointer to the pointer of the first row of the grid
+ * @height: Height of the 2d grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		i++;
+		free(grid[i]);
+	}
+	free(grid);
+}
