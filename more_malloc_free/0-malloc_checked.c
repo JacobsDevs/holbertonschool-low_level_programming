08af29bab1d4b9134f8e8e88a2fb14ffce804194
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-11 00:25:50 +0000

    add: 0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - returns a pointer to the allocated section of memory
+ * @b: number of bytes to allocate
+ *
+ * Return: Pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m = malloc(b);
+	if (m == NULL)
+		exit(98);
+	else
+		return (m);
+}
