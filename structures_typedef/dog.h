057c036f0336af1a9bab2ec10a59ae7326d6069f
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-13 00:48:23 +0000

    add: dog struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - A fluffy friend
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: a Dog that has a name, age and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
