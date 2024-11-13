c0537f6a2104a1219f1d88ceb20eef2564dda524
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-13 00:56:53 +0000

    fix: dog.h protected from double inclusion

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - Intitializes a dog struct with the given parameters.
+ * @d: Pointer to the dog struct
+ * @name: dog's desired name
+ * @age: dog's desired age
+ * @owner: dog's desired owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
