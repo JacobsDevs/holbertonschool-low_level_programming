bea0d8ff6211dcae069498dd6f77b49fd79cc7f7
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-15 05:23:52 +0000

    fix: 5-free_dog.c check nulls

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,7 +7,11 @@
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d);
-	free(d->name);
-	free(d->owner);
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
 }
