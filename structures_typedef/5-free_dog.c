b24bb1500d1c8d73d6fb4306bfc39baa6a1bd394
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-15 05:28:24 +0000

    fix: 5-free_dog.c check if dog exists and free in correct order

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,11 +7,12 @@
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d);
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
-}
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+	}
