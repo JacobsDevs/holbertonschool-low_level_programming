65305b8ff543e8919340d200299d4e43543668ef
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-18 23:21:20 +0000

    add: 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,9 @@
 /**
- * Test
+ * print_name - prints a name in the way the f function wants to be printed
+ * @name: Pointer to the name to print.
+ * @f: Pointer to the function to use to print the name.
  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
