2bdf429ee45639e41371292ee76651697e2e08cf
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-19 01:41:05 +0000

    add: 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - Return the index of an array where an int exists with a function
+ * pointer.
+ * @array: Pointer to the array to iterate through
+ * @size: Size of the array to search
+ * @cmp: Function to use to search
+ *
+ * Return: -1 for no matches or negative size, element of array for found.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size < 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
