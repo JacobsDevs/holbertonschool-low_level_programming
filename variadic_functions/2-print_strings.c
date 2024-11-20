130e3bf4a608ca4b4600a341628cef08a99bb0d1
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-20 02:15:42 +0000

    add: 2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints all the args as strings with the specified separator
+ * @separator: The separator to use
+ * @n: The number of args
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	va_start(ap, n);
+	if (n == 0)
+		return;
+	while (i < n - 1)
+	{
+		if (separator == NULL)
+			printf("%s", va_arg(ap, char *));
+		else
+			printf("%s%s", va_arg(ap, char *), separator);
+		i++;
+	}
+	printf("%s\n", va_arg(ap, char *));
+	va_end(ap);
+}
