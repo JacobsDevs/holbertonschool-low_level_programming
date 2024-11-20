1a564ed37ee7e50c9808fc40731c7dfa91411dcf
JacobsDevs <jacobsdevsmail@gmail.com>
2024-11-20 03:40:28 +0000

    add: 3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - parses through the args and prints them in their type
+ * @ptypes: Key to the type each arg will be
+ */
+
+void print_all(char *ptypes, ...)
+{
+	va_list ap;
+	va_list *apptr = &ap;
+	int i = 0;
+	void (*func_ptr)(va_list *apptr);
+
+	va_start(ap, ptypes);
+	while (ptypes[i] != '\0')
+	{
+		func_ptr = get_parser_func(ptypes[i]);
+		if (func_ptr != NULL)
+		{
+			func_ptr(apptr);
+			if (ptypes[i + 1] == '\0')
+				printf("\n");
+			else
+				printf(", ");
+		}
+		i++;
+	}
+}
+
+/**
+ * get_parser_func - Returns the function pointer to the approporiate print
+ * function.
+ * @s: Character of the ptype
+ *
+ * Return: a pointer to the function you want to use.
+ */
+void (*get_parser_func(char s))(va_list *ap)
+{
+	parser_t parser[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (parser[i].ptype != NULL)
+	{
+		if (parser[i].ptype[0] == s)
+		{
+			return (parser[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * print_char - Prints a char to the screen
+ * @ap: va_list to read
+ */
+
+void print_char(va_list *ap)
+{
+	printf("%c", va_arg(*ap, int));
+}
+
+/**
+ * print_int - Prints an int to the screen
+ * @ap: va_list to read
+ */
+
+void print_int(va_list *ap)
+{
+	printf("%d", va_arg(*ap, int));
+}
+
+/**
+ * print_float - Prints a float to the screen
+ * @ap: va_list to read
+ */
+
+void print_float(va_list *ap)
+{
+	printf("%f", va_arg(*ap, double));
+}
+
+/**
+ * print_string - Prints a string to the screen
+ * @ap: va_list to read
+ */
+
+void print_string(va_list *ap)
+{
+	printf("%s", va_arg(*ap, char *));
+}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(char *ptypes, ...);
+/**
+ * struct parser
+ * @ptype: The type to parse
+ * @f: The function associated
+ */
+typedef struct parser
+{
+	char *ptype;
+	void (*f)(va_list *ap);
+} parser_t;
+void print_char(va_list *ap);
+void print_int(va_list *ap);
+void print_float(va_list *ap);
+void print_string(va_list *ap);
+void (*get_parser_func(char s))(va_list *);
 
 #endif
