151b233b24960972d4deb013cdee6cee5d719b90
JacobsDevs <jacobsdevsmail@gmail.com>
2024-12-02 12:22:30 +1100

    add: 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - prints each item in the linked list and returns the list length
+* @head: the head of the linked list to print
+* 
+* Return: The length of the list
+*/
+
+int print_list(list_t *head)
+{
+	int i = 0;
+	list_t *current = head;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			current->str = "(nil)";
+			current->len = 0;
+		}
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
