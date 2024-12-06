81d8e82e772dc3ee14b18a28f50a88234c2bc3ed
JacobsDevs <jacobsdevsmail@gmail.com>
2024-12-06 15:26:21 +1100

    refactor: 2-get_bit.c handle out of range

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,29 +12,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return (n >> index & 1);
-/*	char *num = malloc(sizeof(char) * 64);
-	unsigned int i = 0;
-
 	if (index > 63)
 		return (-1);
-	while (i < 64)
-	{
-		int result = n >> (63 - i);
-
-		if (result & 1)
-		{
-			num[i] = '1';
-		}
-		else
-		{
-			num[i] = '0';
-		}
-		i++;
-	}
-	if (num[i - (index + 1)] == 49)
-		return (1);
-	else
-		return (0);*/
+	return (n >> index & 1);
 }
 
